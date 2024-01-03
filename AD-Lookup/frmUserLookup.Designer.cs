db5a44bc668a51230bc98b89e4db02e9e185f62e
John McCourt <john.mccourt000@gmail.com>
2024-01-03 14:41:27 +0000

    initial commit

---
+++ AD-Lookup/frmUserLookup.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace AD_Lookup
+{
+    partial class frmUserLookup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtUserLookupReponse = new System.Windows.Forms.TextBox();
+            this.txtNewPass = new System.Windows.Forms.TextBox();
+            this.txtNewPassConfirm = new System.Windows.Forms.TextBox();
+            this.CmdResetPass = new System.Windows.Forms.Button();
+            this.cmdUnlockUser = new System.Windows.Forms.Button();
+            this.cmdShowInactiveUsers = new System.Windows.Forms.Button();
+            this.lstUsers = new System.Windows.Forms.ListBox();
+            this.lstTemp = new System.Windows.Forms.ListBox();
+            this.cmdLookupUser = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(4, 6);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(280, 22);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            this.txtUsername.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUsername_KeyPress);
+            // 
+            // txtUserLookupReponse
+            // 
+            this.txtUserLookupReponse.Location = new System.Drawing.Point(297, 42);
+            this.txtUserLookupReponse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUserLookupReponse.Multiline = true;
+            this.txtUserLookupReponse.Name = "txtUserLookupReponse";
+            this.txtUserLookupReponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUserLookupReponse.Size = new System.Drawing.Size(812, 626);
+            this.txtUserLookupReponse.TabIndex = 3;
+            // 
+            // txtNewPass
+            // 
+            this.txtNewPass.Location = new System.Drawing.Point(8, 30);
+            this.txtNewPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNewPass.Name = "txtNewPass";
+            this.txtNewPass.PasswordChar = '*';
+            this.txtNewPass.Size = new System.Drawing.Size(247, 22);
+            this.txtNewPass.TabIndex = 4;
+            // 
+            // txtNewPassConfirm
+            // 
+            this.txtNewPassConfirm.Location = new System.Drawing.Point(8, 57);
+            this.txtNewPassConfirm.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNewPassConfirm.Name = "txtNewPassConfirm";
+            this.txtNewPassConfirm.PasswordChar = '*';
+            this.txtNewPassConfirm.Size = new System.Drawing.Size(247, 22);
+            this.txtNewPassConfirm.TabIndex = 5;
+            // 
+            // CmdResetPass
+            // 
+            this.CmdResetPass.Location = new System.Drawing.Point(276, 23);
+            this.CmdResetPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CmdResetPass.Name = "CmdResetPass";
+            this.CmdResetPass.Size = new System.Drawing.Size(163, 59);
+            this.CmdResetPass.TabIndex = 6;
+            this.CmdResetPass.Text = "Set New Password";
+            this.CmdResetPass.UseVisualStyleBackColor = true;
+            this.CmdResetPass.Click += new System.EventHandler(this.CmdResetPass_Click);
+            // 
+            // cmdUnlockUser
+            // 
+            this.cmdUnlockUser.Location = new System.Drawing.Point(4, 681);
+            this.cmdUnlockUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdUnlockUser.Name = "cmdUnlockUser";
+            this.cmdUnlockUser.Size = new System.Drawing.Size(283, 94);
+            this.cmdUnlockUser.TabIndex = 8;
+            this.cmdUnlockUser.Text = "Unlock User";
+            this.cmdUnlockUser.UseVisualStyleBackColor = true;
+            this.cmdUnlockUser.Click += new System.EventHandler(this.cmdUnlockUser_Click);
+            // 
+            // cmdShowInactiveUsers
+            // 
+            this.cmdShowInactiveUsers.Location = new System.Drawing.Point(437, 6);
+            this.cmdShowInactiveUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdShowInactiveUsers.Name = "cmdShowInactiveUsers";
+            this.cmdShowInactiveUsers.Size = new System.Drawing.Size(160, 28);
+            this.cmdShowInactiveUsers.TabIndex = 9;
+            this.cmdShowInactiveUsers.Text = "Show Inactive Users";
+            this.cmdShowInactiveUsers.UseVisualStyleBackColor = true;
+            this.cmdShowInactiveUsers.Click += new System.EventHandler(this.cmdShowInactiveUsers_Click);
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FormattingEnabled = true;
+            this.lstUsers.ItemHeight = 16;
+            this.lstUsers.Location = new System.Drawing.Point(4, 39);
+            this.lstUsers.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(280, 628);
+            this.lstUsers.TabIndex = 10;
+            this.lstUsers.SelectedIndexChanged += new System.EventHandler(this.lstUsers_SelectedIndexChanged);
+            // 
+            // lstTemp
+            // 
+            this.lstTemp.ForeColor = System.Drawing.Color.Transparent;
+            this.lstTemp.FormattingEnabled = true;
+            this.lstTemp.ItemHeight = 16;
+            this.lstTemp.Location = new System.Drawing.Point(773, 896);
+            this.lstTemp.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstTemp.Name = "lstTemp";
+            this.lstTemp.Size = new System.Drawing.Size(159, 100);
+            this.lstTemp.TabIndex = 11;
+            this.lstTemp.SelectedIndexChanged += new System.EventHandler(this.lstTemp_SelectedIndexChanged);
+            // 
+            // cmdLookupUser
+            // 
+            this.cmdLookupUser.Location = new System.Drawing.Point(296, 6);
+            this.cmdLookupUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdLookupUser.Name = "cmdLookupUser";
+            this.cmdLookupUser.Size = new System.Drawing.Size(129, 28);
+            this.cmdLookupUser.TabIndex = 14;
+            this.cmdLookupUser.Text = "Look Up User";
+            this.cmdLookupUser.UseVisualStyleBackColor = true;
+            this.cmdLookupUser.Click += new System.EventHandler(this.cmdLookupUser_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.CmdResetPass);
+            this.groupBox1.Controls.Add(this.txtNewPass);
+            this.groupBox1.Controls.Add(this.txtNewPassConfirm);
+            this.groupBox1.Location = new System.Drawing.Point(301, 674);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(452, 100);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PW Reset";
+            // 
+            // frmUserLookup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 782);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmdLookupUser);
+            this.Controls.Add(this.lstTemp);
+            this.Controls.Add(this.lstUsers);
+            this.Controls.Add(this.cmdShowInactiveUsers);
+            this.Controls.Add(this.cmdUnlockUser);
+            this.Controls.Add(this.txtUserLookupReponse);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmUserLookup";
+            this.Text = "User Lookup Tool";
+            this.Load += new System.EventHandler(this.user_lookup_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtUserLookupReponse;
+        private System.Windows.Forms.TextBox txtNewPass;
+        private System.Windows.Forms.TextBox txtNewPassConfirm;
+        private System.Windows.Forms.Button CmdResetPass;
+        private System.Windows.Forms.Button cmdUnlockUser;
+        private System.Windows.Forms.Button cmdShowInactiveUsers;
+        private System.Windows.Forms.ListBox lstUsers;
+        private System.Windows.Forms.ListBox lstTemp;
+        private System.Windows.Forms.Button cmdLookupUser;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ AD-Lookup/frmUserLookup.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AD_Lookup
+{
+    public partial class frmUserLookup : Form
+    {
+        public frmUserLookup()
+        {
+            InitializeComponent();
+        }
+
+        private void user_lookup_Load(object sender, EventArgs e)
+        {
+       //     ListUsers();
+        }
+
+        private void cmdUserLookup_Click(object sender, EventArgs e)
+        {
+           
+
+        //  DisplayUserInfo()
+        }
+
+        private void CmdResetPass_Click(object sender, EventArgs e)
+        {
+            if (txtNewPass.Text == txtNewPassConfirm.Text)
+            {
+
+                //net user /domain USERNAME NEWPASS
+
+                String CmdText = @"user /domain " + txtUsername.Text + " "+txtNewPass.Text;
+
+                Process proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "net.exe",
+                        Arguments = CmdText,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                proc.Start();
+                String cmdOutput = proc.StandardOutput.ReadToEnd();
+                MessageBox.Show("The password for this user has been reset");
+
+            }
+
+            else
+            {
+                MessageBox.Show("Passwords didn't match");   
+             }
+        }
+
+        private void cmdUnlockUser_Click(object sender, EventArgs e)
+        {
+            //   Net user loginid / ACTIVE:YES / domain
+
+
+
+  
+
+            String CmdText = @"user " + txtUsername.Text + " /ACTIVE:YES  /domain";
+
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "net.exe",
+                    Arguments = CmdText,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            String cmdOutput = proc.StandardOutput.ReadToEnd();
+            MessageBox.Show("The user has been unlocked");
+
+
+        }
+
+        private void cmdShowInactiveUsers_Click(object sender, EventArgs e)
+        {
+
+
+            //Clear the list of users
+            lstUsers.Items.Clear();
+            //Get all the users
+            //NET USERS / DOMAIN > USERS.TXT
+
+
+
+
+
+            String finalOutput ="";
+            String CmdText = @"users  /domain";
+            //Message
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "net.exe",
+                    Arguments = CmdText,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            String cmdOutput = proc.StandardOutput.ReadToEnd();
+
+
+            cmdOutput = RemoveFirstLines(cmdOutput, 8);
+
+            //Now parse out the names
+            using (StringReader reader = new StringReader(cmdOutput))
+            {
+                string line = string.Empty;
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        // do something with the line
+
+                        string[] adusers = line.Split(' ');
+
+                        foreach (var aduser in adusers)
+                        {
+                            if(!(aduser==""))
+                            {
+                                System.Console.WriteLine($"<{aduser}>");
+                                finalOutput = finalOutput + "\r\n" + aduser;
+
+                                if (CheckIfUserIsInActive(aduser))
+                                    {
+
+                                    lstUsers.Items.Add(aduser);
+                                    }//end check to see if the user is
+                            }
+               
+                        }
+
+
+
+                    }
+
+                } while (line != null);
+            }
+
+
+
+            lstUsers.Sorted = true;
+
+            txtUserLookupReponse.Text = finalOutput;
+
+
+        }
+
+
+
+        public static bool CheckIfUserIsInActive(string aduser) {
+
+
+            //find out when the user last logged itn
+            //net user administrator | findstr / B / C:"Last logon"
+            String finalOutput = "";
+            String CmdText = @"users  /domain";
+
+
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "net.exe",
+                    Arguments = CmdText,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            String cmdOutput = proc.StandardOutput.ReadToEnd();
+
+
+            cmdOutput = RemoveFirstLines(cmdOutput, 8);
+
+
+
+
+            //if the user logged in recentle
+            //return true
+            //else
+            //return false
+
+            return false;
+        
+        }//end check if user is inactive
+
+  
+
+
+        public static string RemoveFirstLines(string text, int linesCount)
+        {
+            var lines = Regex.Split(text, "\r\n|\r|\n").Skip(linesCount);
+            return string.Join(Environment.NewLine, lines.ToArray());
+        }
+
+
+
+
+
+
+
+
+        public void ListUsers()
+        {
+
+            //NET USERS / DOMAIN > USERS.TXT
+            String finalOutput = "";
+            String CmdText = @"users  /domain";
+            //Message
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "net.exe",
+                    Arguments = CmdText,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            String cmdOutput = proc.StandardOutput.ReadToEnd();
+
+
+            cmdOutput = RemoveFirstLines(cmdOutput, 8);
+
+            //Now parse out the names
+            using (StringReader reader = new StringReader(cmdOutput))
+            {
+                string line = string.Empty;
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        // do something with the line
+
+                        string[] words = line.Split(' ');
+
+                        foreach (var word in words)
+                        {
+                            if (!(word == ""))
+                            {
+                                //      System.Console.WriteLine($"<{word}>");
+                                //     finalOutput = finalOutput + "\r\n" + word;
+                              
+                                lstUsers.Items.Add(word);
+                                lstTemp.Items.Add(word);
+
+                            }
+
+                        }
+
+
+
+                    }
+
+                } while (line != null);
+            }
+
+
+
+
+
+
+        }//end ListUsers
+
+
+
+
+
+        public void DisplayUser(string username)
+        {
+
+            String CmdText = @"user " + username + " /domain";
+            //Message
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "net.exe",
+                    Arguments = CmdText,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            String cmdOutput = proc.StandardOutput.ReadToEnd();
+            txtUserLookupReponse.Text = cmdOutput;
+
+        }//end void displayuser
+
+        private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayUser(lstUsers.SelectedItem.ToString());
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void txtUsername_KeyPress(object sender, KeyPressEventArgs e)
+        {
+     
+            SearchByUser();
+        }
+
+      private void SearchByUser() {
+
+            lstUsers.Items.Clear();
+            for (int i = lstTemp.Items.Count - 1; i >= 0; i--)
+            {
+
+                if (lstTemp.Items[i].ToString().Contains(txtUsername.Text.ToString()))
+                {
+                    String[] strPieces = lstTemp.Items[i].ToString().Split('|');
+                    lstUsers.Items.Add(strPieces[0]);
+                  
+                }//end if
+            }
+
+            lstUsers.Sorted= true;
+
+        }//end void
+
+        private void lstTemp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void cmdLookupUser_Click(object sender, EventArgs e)
+        {
+            DisplayUser(txtUsername.Text);
+
+        }
+
+    
+        
+    }
+}
